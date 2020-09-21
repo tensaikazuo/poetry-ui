9ba4c97eff7015082f2d7ebddc7ca2f95a6dbbbf
Kazuo ARAKI <tensaikazuo@hotmail.com>
2020-09-21 19:13:49 +0900

    Initial commit, MUI added to the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  title: {
+    textAlign: 'center',
+  },
+  entry: {
+    width: '600px',
+    margin: '0 auto',
+    '& textarea': {
+      width: '100%',
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <div className={classes.title}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Poetry-UI
+        </Typography>
+      </div>
+      <div className={classes.entry}>
+        <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Please write your work..." />
+      </div>
+    </>
   );
 }
 
