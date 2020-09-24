e4bfcd4b85d7975a4856c77a93efe12a0cd8b2f0
Kazuo ARAKI <tensaikazuo@hotmail.com>
2020-09-24 23:10:53 +0900

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { StateMachineProvider, createStore } from 'little-state-machine';
+import Input from './Input';
+import Output from './Output';
 
-const useStyles = makeStyles({
-  title: {
-    textAlign: 'center',
-  },
-  entry: {
-    width: '600px',
-    margin: '0 auto',
-    '& textarea': {
-      width: '100%',
-    },
-  },
-  button: {
-    textAlign: 'center',
-  },
-  mbsm: {
-    marginBottom: '8px',
-  },
-  mbmd: {
-    marginBottom: '16px',
-  }
+createStore({
+  data: {}
 });
 
 function App() {
-  const classes = useStyles();
   return (
-    <>
-      <CssBaseline />
-      <div className={classes.title}>
-        <Typography variant="h2" component="h1">
-          Poetry-UI
-        </Typography>
-      </div>
-      <div className={`${classes.entry} ${classes.mbsm}`}>
-        <TextField label="Title" className={classes.mbmd} />
-        <TextareaAutosize aria-label="minimum height" rowsMin={10} placeholder="Please write your work..." />
-      </div>
-      <div className={classes.button}>
-        <Button variant="outlined">Preview</Button>
-      </div>
-    </>
+    <StateMachineProvider>
+      <Router>
+        <Route exact path="/" component={Input} />
+        <Route path="/output" component={Output} />
+      </Router>
+    </StateMachineProvider>
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  title: {
+    textAlign: 'center',
+  },
+  entry: {
+    width: '600px',
+    margin: '0 auto',
+    '& textarea': {
+      width: '100%',
+    },
+  },
+  button: {
+    textAlign: 'center',
+  },
+  mbsm: {
+    marginBottom: '8px',
+  },
+  mbmd: {
+    marginBottom: '16px',
+  }
+});
+
+function Input() {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <div className={classes.title}>
+        <Typography variant="h2" component="h1">
+          Poetry-UI
+        </Typography>
+      </div>
+      <div className={`${classes.entry} ${classes.mbsm}`}>
+        <TextField label="Title" className={classes.mbmd} />
+        <TextareaAutosize aria-label="minimum height" rowsMin={10} placeholder="Please write your work..." />
+      </div>
+      <div className={classes.button}>
+        <Button variant="outlined">Preview</Button>
+      </div>
+    </>
+  );
+}
+
+export default Input;
